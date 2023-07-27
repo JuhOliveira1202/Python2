dc5f6cad4c060073052d06a4993d5dfb646aea43
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-27 16:15:55 +0100

    Add files via upload

---
+++ Exercise 5 - Logical Operations.py
@@ -0,0 +1,23 @@
+#Exercício 5:
+#Elabora um programa que calcule o valor lógico das seguintes proposições
+
+#4==5
+calculo1 = 4==5
+print(calculo1)
+
+#4 diferente 6
+calculo2 = 4!=5
+print(calculo2)
+      
+#4 menor que 5 e 6 maior que 10
+calculo3 = 4<5 and 6>10
+print(calculo3)
+
+#40 menor que 50 ou 60 maior ou igual que 90
+calculo4 = 40<50 or 60>=90
+print(calculo4)
+
+#Negação da proposição anterior
+calculo5= not (calculo4)
+print(calculo5)
+
